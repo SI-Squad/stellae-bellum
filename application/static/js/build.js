3f2592292fb7f59e1cbf268ade5ae643a3fbad68
Alex Scholeno <avs7318@g.rit.edu>
2020-09-16 17:49:14 -0400

    commiting the add_ship_to_board function and the corresponding global v

--- application/static/js/build.js
+++ application/static/js/build.js
@@ -1,3 +1,8 @@
+// global variable that is used to hold the cells where ships are placed
+var onboard = Object();
+onboard.shiplist = new Array(5);
+
+
 function getCells(){
     var cells = new Array(10);
     for(var i = 0; i < 10; i++){
@@ -31,7 +36,16 @@ function getBoard(){
         }
     }
     sendJSON(JSON.stringify(board));
-    
+
     console.log(JSON.stringify(board));
 
+}
+
+// function to add a ship with a specific type and it's associated cells to the board, the global variable
+function add_ship_to_board(type,cells){
+    // check validity of cells in future
+    var ship = Object();
+    ship.type = type;
+    ship.cells = cells;
+    window.onboard.shiplist.push(ship);
 }
